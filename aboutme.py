0b1750e8f59d892e21059b0562fc05e26ebbd031
StarloTheSheriff <luminanceswebsite@mail.ru>
2024-06-16 00:58:22 +0300

    Create aboutme.py

---
+++ aboutme.py
@@ -0,0 +1,5 @@
+sheriff_name = "North Star"
+location = "Wild East"
+favorite_game = "Fortnite"
+
+print("Howdy, I'm " + sheriff_name + " from " + location + "! When I'm not keepin' the peace, I love playin' " + favorite_game + "!")
